370ff7777b853ef6e050494092274ea340b7938d
refugiorangel <54346027+refugiorangel@users.noreply.github.com>
2022-04-06 17:49:37 -0500

    Delete js directory

--- js/barra_responsive.js
+++
@@ -1,6 +0,0 @@
-const enlaces_barra = document.querySelector(".enlaces_navbar")
-const boton_barra = document.querySelector(".boton_barra")
-
-boton_barra.addEventListener("click", () => {
-    enlaces_barra.classList.toggle("barra_menu_visible")
-})
