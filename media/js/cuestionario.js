9327993b8dbe66bbe555e46cd5f0db63fc29cb58
AlexVilleC <103164070+AlexVilleC@users.noreply.github.com>
2022-04-13 19:38:09 -0500

    Update cuestionario.js

--- media/js/cuestionario.js
+++ media/js/cuestionario.js
@@ -40,3 +40,4 @@ preguntas[9]=new pregunta("¿En cuántas partes se divide la organización inter
 preguntas[10]=new pregunta("¿Cuántas cintas tiene una MT que representa a U?" , "3" , "4" , "5" , "6" , 2 );
 preguntas[11]=new pregunta("¿De cuántas fases se compone la actividad de U?" , "1" , "2" , "3" , "4" , 3 );
 preguntas[12]=new pregunta("¿A qué teorema pertenece el siguiente enunciado?.Existe una función calculable parcial de dos argumentos ϕz tal que, para todo x e i, ϕz(i, x) = ϕi(x) " , "universalidad" , "cardinalidad" , "complejidad" , "computabilidad" , 1 );
+preguntas[13]=new pregunta("Dentro de la siguiente representación U〈φ,x〉=φi(x).¿A qué corresponde φ?" , "función universal" , "la entrada" , "conjunto de funciones" , "la salida" , 3 );
